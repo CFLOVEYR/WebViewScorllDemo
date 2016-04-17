01cc9b22ad41d3194d7958fc6e6cfc52fc14d3ea
Jalen <1439504683@qq.com>
2016-04-17 17:45:12 +0800

    注释优化

--- app/src/main/java/com/tofirst/jalen/webviewscorlldemo/MainActivity.java
+++ app/src/main/java/com/tofirst/jalen/webviewscorlldemo/MainActivity.java
@@ -9,7 +9,9 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
-
+/**
+ * 此工程是Android Studio 2.0建立的 需要gradle2.10才可以
+ */
 public class MainActivity extends AppCompatActivity {
     private CoordinatorLayout mainContent;
     private AppBarLayout appbar;
@@ -22,16 +24,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
-
+        initWebView();
     }
 
-
+    /**
+     * 初始化布局
+     */
     private void initView() {
         mainContent = (CoordinatorLayout) findViewById(R.id.main_content);
         appbar = (AppBarLayout) findViewById(R.id.appbar);
-        toolBar = (Toolbar) findViewById(R.id.toolBar);
+        toolBar = (Toolbar) findViewById(R.id.toolBar);//
         webview = (WebView) findViewById(R.id.webview);
 
+    }
+
+    /**
+     * 初始化WebView的配置
+     */
+    private void initWebView() {
         webview.setWebViewClient(new WebViewClient() {
             //设置在webView点击打开的新网页在当前界面显示,而不跳转到新的浏览器中
             @Override
@@ -40,9 +50,10 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-        webview.getSettings().setJavaScriptEnabled(true);  //设置WebView属性,运行执行js脚本
-        webview.loadUrl("http://android.jobbole.com/82375/");          //调用loadView方法为WebView加入链接
-//        setContentView(webview);                           //调用Activity提供的setContentView将webView显示出来
+        //设置WebView属性,运行执行js脚本
+        webview.getSettings().setJavaScriptEnabled(true);
+        //调用loadView方法为WebView加入链接
+        webview.loadUrl("http://blog.csdn.net/y1258429182");
     }
 
 
